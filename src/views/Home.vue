136cf87797b0f0cec73ebbf5f030591fc25da687
oneut <o.l.c.neut@student.tudelft.nl>
2020-05-12 23:10:33 +0200

    Added router

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <ContentPage/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ContentPage from '@/components/ContentPage.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    ContentPage
+  }
+}
+</script>
