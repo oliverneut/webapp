0ec492eec3d4f27e4dcc9f7b448c41bacb5caa31
oneut <o.l.c.neut@student.tudelft.nl>
2020-05-20 18:32:13 +0200

    added new views

---
+++ src/views/Projects.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Projects">
+    <h1>This is an about page dneunwhu</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
